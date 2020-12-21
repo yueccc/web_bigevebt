7c160fcf0b3fb67104d953477afce411c678ecf4
yue <2649160158@qq.com>
2020-12-21 14:42:12 +0800

    完成了index首页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,19 @@
 $.ajaxPrefilter(function (options) {
     // 每次通过jQuery发起ajax请求之前，都会调用这个函数
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // 自动添加headers数据
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function (res) {
+        var errormsg = JSON.parse(res.responseText);
+        if (errormsg.status == 1 && errormsg.message == '身份认证失败！') {
+            // 清空loken  强行跳转到login
+            localStorage.removeItem('token')
+            location.href = './../../login.html'
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    // 进入首页发起Ajax请求 获得数据
+    $.ajax({
+        method: "get",
+        url: "/my/userinfo",
+        success: function (res) {
+            if (res.status !== 0) {
+                layui.layer.msg('获取用户数据失败')
+            } else {
+                var username = res.data.nickname || res.data.username
+                // 生成欢迎xxx
+                $('.welcome').html('欢迎&nbsp ' + username)
+                setAvater(res.data, username)
+            }
+        }
+    });
+    // 退出功能
+    $('#btnLogout').on('click', function () {
+        // 清空本地储存  跳转login
+        layui.layer.confirm('是否确认退出?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = './../../login.html'
+            layer.close(index);
+        });
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+// 渲染头像
+function setAvater(user, username) {
+    var pic = user.user_pic;
+    if (pic == null) {
+        var first = username[0].toUpperCase()
+        $('.text-avater').html(first).show();
+        $('.layui-nav-img').hide();
+    } else {
+        $('.text-avater').hide();
+        $('.layui-nav-img').attr(src, pic).show();
+    }
+}
